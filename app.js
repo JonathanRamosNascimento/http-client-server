11d28f1516e253402c87ec91f3046232c07a5fe2
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-06 13:26:50 -0300

    new method for editing products

--- app.js
+++ app.js
@@ -111,4 +111,27 @@ app.delete('/products/:id', function (req, res) {
         })
 });
 
+app.path('/products/:id', function (req, res) {
+    Product.findById(req.params.id, (err, prod) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        else if (!prod) {
+            res.status(404).send({});
+        }
+        else {
+            prod.name = req.body.name;
+            prod.price = req.body.price;
+            prod.department = req.body.department;
+            prod.save((err, prod) => {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(200).send(prod);
+                }
+            });
+        }
+    })
+});
+
 app.listen(3000);
